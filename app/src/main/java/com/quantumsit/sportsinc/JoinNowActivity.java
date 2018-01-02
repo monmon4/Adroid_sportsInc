ddcd049cc2db636e83ae77ddf73907a941382047
Mona <mona_ahmed_94@hotmail.com>
2018-01-02 15:54:43 +0200

    "activity join now created"

---
+++ app/src/main/java/com/quantumsit/sportsinc/JoinNowActivity.java
@@ -0,0 +1,13 @@
+package com.quantumsit.sportsinc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinNowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_now);
+    }
+}
