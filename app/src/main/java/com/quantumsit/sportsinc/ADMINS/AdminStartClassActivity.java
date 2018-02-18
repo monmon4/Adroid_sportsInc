ebc68844dcb737be78391b90980c04e3b480d871
Mona <mona_ahmed_94@hotmail.com>
2018-02-18 17:00:05 +0200

    "continue reassign coach"

--- app/src/main/java/com/quantumsit/sportsinc/ADMINS/AdminStartClassActivity.java
+++ app/src/main/java/com/quantumsit/sportsinc/ADMINS/AdminStartClassActivity.java
@@ -11,13 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.quantumsit.sportsinc.Aaa_data.Constants;
@@ -25,6 +28,7 @@ import com.quantumsit.sportsinc.Backend.HttpCall;
 import com.quantumsit.sportsinc.Backend.HttpRequest;
 import com.quantumsit.sportsinc.CustomCalendar.ListViewAdapter;
 import com.quantumsit.sportsinc.R;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,10 +49,14 @@ public class AdminStartClassActivity extends AppCompatActivity {
 
     PopupWindow coach_reassign_popup_window;
     private Context admin_start_class_Context;
-    private RelativeLayout admin_start_class_rl;
+    private LinearLayout admin_start_class_rl;
 
     ProgressDialog progressDialog;
     List<String> coach_names_list;
+    //ArrayAdapter<String> reassign_spinner_adapter;
+
+    MaterialBetterSpinner reassign_spinner;
+    String[] empty = {""};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,8 +65,9 @@ public class AdminStartClassActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(AdminStartClassActivity.this);
         progressDialog.setMessage("Please wait......");
 
-        admin_start_class_Context = getApplicationContext();
-        admin_start_class_rl =  findViewById(R.id.rl_adminstartclass);
+        admin_start_class_Context = AdminStartClassActivity.this;
+        admin_start_class_rl =  findViewById(R.id.ll_adminstartclass);
+        reassign_spinner =findViewById(R.id.reassignSpinner);
 
         note_editText = findViewById(R.id.attendancenotesEditText_admincurrentclass2);
         coach_name_checkBox = findViewById(R.id.coachNameCheckBox_admincurrentclass2);
@@ -74,6 +83,11 @@ public class AdminStartClassActivity extends AppCompatActivity {
         attend = 0;
 
         coach_names_list = fill_coach_names_list();
+
+
+        ArrayAdapter<String> reassign_spinner_adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line,empty);
+        //reassign_spinner_adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        reassign_spinner.setAdapter(reassign_spinner_adapter);
 
         item_current_classes classes = (item_current_classes) getIntent().getSerializableExtra("adminClass");
 
@@ -154,7 +168,19 @@ public class AdminStartClassActivity extends AppCompatActivity {
         return false;
     }
 
+    @Override
+    public void onBackPressed() {
+        if (coach_reassign_popup_window != null && coach_reassign_popup_window.isShowing()) {
+            coach_reassign_popup_window.dismiss();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     public void reassign_clicked() {
+
+        note_editText.setEnabled(false);
+
 
         LayoutInflater inflater = (LayoutInflater) admin_start_class_Context.getSystemService(LAYOUT_INFLATER_SERVICE);
         View customView = inflater.inflate(R.layout.window_reassign_coach_layout,null);
@@ -169,24 +195,41 @@ public class AdminStartClassActivity extends AppCompatActivity {
             coach_reassign_popup_window.setElevation(5.0f);
         }
 
-        ListView coach_reassign_listView = customView.findViewById(R.id.coachesListView);
+        final ListView coach_reassign_listView = customView.findViewById(R.id.coachesListView);
 
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, coach_names_list);
+        final ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(this,  android.R.layout.simple_list_item_1, coach_names_list);
 
         coach_reassign_listView.setAdapter(itemsAdapter);
-
         coach_reassign_popup_window.showAtLocation(admin_start_class_rl, Gravity.CENTER,0,0);
-        coach_reassign_popup_window.setFocusable(true);
-        coach_reassign_popup_window.setOutsideTouchable(false);
-        coach_reassign_popup_window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        coach_reassign_popup_window.setFocusable(false);
+        coach_reassign_popup_window.setOutsideTouchable(true);
         coach_reassign_popup_window.update();
+
+        coach_reassign_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TextView textView = view.findViewById(R.id.textView7);
+                //String selected_coach = textView.getText().toString();
+                String selected_coach = coach_reassign_listView.getItemAtPosition(position).toString();
+                Toast.makeText(AdminStartClassActivity.this, selected_coach, Toast.LENGTH_SHORT).show();
+                coach_name_checkBox.setText(selected_coach);
+                coach_names_list = fill_coach_names_list();
+                itemsAdapter.notifyDataSetChanged();
+                note_editText.setEnabled(true);
+                coach_reassign_popup_window.dismiss();
+            }
+        });
+
 
     }
 
     @SuppressLint("StaticFieldLeak")
     private List<String> fill_coach_names_list()  {
-        final List<String> coach_names_list = new ArrayList<>();
+
+        progressDialog.show();
+
+        final List<String> coach_names_list2 = new ArrayList<>();
 
         JSONObject where_info = new JSONObject();
 
@@ -211,11 +254,19 @@ public class AdminStartClassActivity extends AppCompatActivity {
                             try {
                                 JSONObject result = response.getJSONObject(i);
                                 String coach = result.getString("name");
-                                coach_names_list.add(coach);
+                                if(!coach.equals(coach_name_checkBox.getText().toString()))
+                                    coach_names_list2.add(coach);
+
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                         }
+
+                        //reassign_spinner_adapter.notifyDataSetChanged();
+                        //reassign_spinner.notify();
+                        ArrayAdapter<String> reassign_spinner_adapter = new ArrayAdapter<>(AdminStartClassActivity.this, android.R.layout.simple_dropdown_item_1line,coach_names_list2);
+                        reassign_spinner.setAdapter(reassign_spinner_adapter);
+                        progressDialog.dismiss();
 
                     } else {
                         Toast.makeText(AdminStartClassActivity.this,"An error occurred",Toast.LENGTH_SHORT);
@@ -227,7 +278,7 @@ public class AdminStartClassActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        return coach_names_list;
+        return coach_names_list2;
     }
 
 }
