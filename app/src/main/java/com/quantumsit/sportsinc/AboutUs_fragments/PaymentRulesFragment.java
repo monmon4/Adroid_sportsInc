67cd7888b5039b8500f6bb229f66cf8f9fdd38cf
Mona <mona_ahmed_94@hotmail.com>
2018-03-04 12:19:19 +0200

    "about us dynamic"

--- app/src/main/java/com/quantumsit/sportsinc/AboutUs_fragments/PaymentRulesFragment.java
+++ app/src/main/java/com/quantumsit/sportsinc/AboutUs_fragments/PaymentRulesFragment.java
@@ -1,24 +1,102 @@
 package com.quantumsit.sportsinc.AboutUs_fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.quantumsit.sportsinc.Aaa_data.Constants;
+import com.quantumsit.sportsinc.Backend.HttpCall;
+import com.quantumsit.sportsinc.Backend.HttpRequest;
 import com.quantumsit.sportsinc.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class PaymentRulesFragment extends Fragment {
+
+    ListView listView;
+    ListView_Adapter_about_us listView_adapter;
+
+    ProgressDialog progressDialog;
+
+    ArrayList<item_about> items;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_payment_rules,container,false);
 
+        listView = root.findViewById(R.id.listView_paymentRules);
+        items = new ArrayList<>();
+
+        progressDialog= new ProgressDialog(getContext());
+        progressDialog.setMessage("Please wait");
+
+        progressDialog.show();
+        get_list_items();
+
+        listView_adapter = new ListView_Adapter_about_us(getContext(), items);
+        listView.setAdapter(listView_adapter);
+
 
         return root;
+    }
+
+    private void get_list_items(){
+
+        JSONObject where_info = new JSONObject();
+        try {
+            where_info.put("type",1);
+
+            HttpCall httpCall = new HttpCall();
+            httpCall.setMethodtype(HttpCall.POST);
+            httpCall.setUrl(Constants.selectData);
+            HashMap<String,String> params = new HashMap<>();
+            params.put("table","rules");
+            params.put("where",where_info.toString());
+
+            httpCall.setParams(params);
+
+            new HttpRequest(){
+                @Override
+                public void onResponse(JSONArray response) {
+                    super.onResponse(response);
+                    fill_list_items(response );
+                }
+            }.execute(httpCall);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fill_list_items (JSONArray response) {
+
+        try {
+            for (int i=0; i<response.length(); i++) {
+                JSONObject result = response.getJSONObject(i);
+                int type = result.getInt("Type");
+                String title = result.getString("Title");
+                String content = result.getString("Content");
+                items.add(new item_about(type, title, content));
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        listView_adapter.notifyDataSetChanged();
+        progressDialog.dismiss();
     }
 
 }
---
+++ app/src/main/java/com/quantumsit/sportsinc/AboutUs_fragments/item_about.java
@@ -0,0 +1,17 @@
+package com.quantumsit.sportsinc.AboutUs_fragments;
+
+/**
+ * Created by Mona on 27-Dec-17.
+ */
+
+public class item_about {
+
+    int type;
+    String title, content;
+
+    public item_about(int type, String title, String content) {
+        this.type = type;
+        this.title = title;
+        this.content = content;
+    }
+}
