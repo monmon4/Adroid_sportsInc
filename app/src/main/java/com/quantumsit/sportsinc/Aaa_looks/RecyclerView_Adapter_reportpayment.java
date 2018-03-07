007c2ff57696302c24d0f2b2209a84d43ec8d1c1
Mona <mona_ahmed_94@hotmail.com>
2018-03-07 11:40:28 +0200

    "fixed some strings"

--- app/src/main/java/com/quantumsit/sportsinc/Aaa_looks/RecyclerView_Adapter_reportpayment.java
+++ app/src/main/java/com/quantumsit/sportsinc/Aaa_looks/RecyclerView_Adapter_reportpayment.java
@@ -45,7 +45,7 @@ public class RecyclerView_Adapter_reportpayment extends RecyclerView.Adapter<Rec
         holder.creation_date.setText(List_Item.get(position).creation_date);
         String payment = "$" + List_Item.get(position).payment;
         holder.payment.setText(payment);
-        String due_date_string = R.string.due +": " + List_Item.get(position).due_date;
+        String due_date_string = String.valueOf(R.string.due) +": " + List_Item.get(position).due_date;
         holder.due_date.setText(due_date_string);
 
         if (List_Item.get(position).status != 0){
