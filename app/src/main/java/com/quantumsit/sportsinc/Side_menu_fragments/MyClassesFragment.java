af1b4067a2eec4d977b9f2eec158d3251bc4ae54
BassamQuantum <b.saber@quantumsit.com>
2018-03-26 15:40:58 +0200

    issues fixing

--- app/src/main/java/com/quantumsit/sportsinc/Side_menu_fragments/MyClassesFragment.java
+++ app/src/main/java/com/quantumsit/sportsinc/Side_menu_fragments/MyClassesFragment.java
@@ -60,10 +60,11 @@ public class MyClassesFragment extends Fragment {
     public void setupViewPager(ViewPager viewPager) {
 
         myclasses_sectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
-        if(trainee){
-            myclasses_sectionsPagerAdapter.addFragment(new ScoresFragment(),getString(R.string.scores));
-        }else if (coach){
+        if (coach){
             myclasses_sectionsPagerAdapter.addFragment(new CoachClassesFragment(),getString(R.string.my_groups));
+        }
+        else {
+            myclasses_sectionsPagerAdapter.addFragment(new ScoresFragment(),getString(R.string.scores));
         }
         myclasses_sectionsPagerAdapter.addFragment(new CalendarFragment(),getString(R.string.calendar));
 
