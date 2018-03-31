14b22383574a6b0023780839e210191f31efdc72
BassamQuantum <b.saber@quantumsit.com>
2018-03-31 14:03:15 +0200

    Limit Connect Time

--- app/src/main/java/com/quantumsit/sportsinc/Backend/HttpRequest.java
+++ app/src/main/java/com/quantumsit/sportsinc/Backend/HttpRequest.java
@@ -44,8 +44,8 @@ public class HttpRequest extends AsyncTask<HttpCall, String, JSONArray> {
             urlConnection = (HttpURLConnection) url.openConnection();
             Log.d(TAG,"Connected");
             urlConnection.setRequestMethod(httpCall.getMethodtype() == HttpCall.GET ? "GET":"POST");
-           // urlConnection.setReadTimeout(10000 /* milliseconds */);
-           // urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
             if(httpCall.getParams() != null && httpCall.getMethodtype() == HttpCall.POST){
 
                 OutputStream os = urlConnection.getOutputStream();
